06fd4dc8ae7ccf0c51dd15d50cec6d68229c6e97
Arun Sathyan <arunsathyan660@gmail.com>
2025-05-16 22:28:55 +0530

    Added different pages and completed Sidebar with dummy user data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import ProfilePage from './pages/ProfilePage'
 
 const App = () => {
   return (
-    <div>
-      React
+    <div className='bg-[url("./src/assets/bgImage.jpg")] bg-contain'>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/profile' element={<ProfilePage />} />
+      </Routes>
     </div>
   )
 }
