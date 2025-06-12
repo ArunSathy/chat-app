4d96aa15eba05135dbb6e2ed6d5c54fde97faafa
Arun Sathyan <arunsathyan660@gmail.com>
2025-06-12 22:36:29 +0530

    Chat Container placeholder updation for specific users

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -91,7 +91,7 @@ const ChatContainer = () => {
       {/* bottom area */}
       <div className='absolute bottom-0 left-0 right-0 flex items-center gap-3 p-3'>
           <div className='flex-1 flex items-center bg-gray-100/12 px-3 rounded-full'>
-            <input onChange={(e) => setInput(e.target.value)} value={input} onKeyDown={(e) => e.key === "Enter" ? handleSendMessage(e) : null} type="text" placeholder='Send message' className='flex-1 text-sm p-3 border-none rounded-lg outline-none text-white placeholder-gray-400'/>
+            <input onChange={(e) => setInput(e.target.value)} value={input} onKeyDown={(e) => e.key === "Enter" ? handleSendMessage(e) : null} type="text" placeholder={`Message ${selectedUser.fullName}`} className='flex-1 text-sm p-3 border-none rounded-lg outline-none text-white placeholder-gray-400'/>
             <input onChange={handleSendImage} type="file" id='image' accept='image/png, image/jpeg' hidden/>
             <label htmlFor="image">
               <img src={assets.gallery_icon} alt="" className='w-5 mr-2 cursor-pointer' />
